2a2a069c965483ed915d0a1f9ead1cba8f9ab870
Denis <74959243+Denrubka@users.noreply.github.com>
2022-05-15 19:21:28 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+if (document.documentElement.clientWidth <= 1170) {
+    $('.programs__slider').slick({
+        infinite: false,
+        arrows: false,
+        centerMode: true,
+        slidesToScroll: 1,
+    });
+}
